265222029a00d750cc0444af4b16a3c4367d9a6e
Evgeniy <evgen-polyanskii@yandex.ru>
2022-12-17 18:05:15 +0300

    Added App component

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../slices/counterSlice.js';
+
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Прибавить
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Отнять
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
